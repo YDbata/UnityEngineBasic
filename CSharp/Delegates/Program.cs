68d54082b937eb3d7afe9fa15674c18ea58fdb67
YDbata <kiz75@naver.com>
2023-10-15 02:12:26 +0900

    feat : 231007 내용 복습

--- CSharp/Delegates/Program.cs
+++ CSharp/Delegates/Program.cs
@@ -21,7 +21,7 @@ namespace Delegates
         public delegate void OnContentUploadedHandler(Content content); // 대리자 type : 함수선언과 같이 returntype name(param)형식으로 선언
 
         // event한정자
-        // 외부에서는 이 대리자의 +=과 -=만 사용가능
+        // 외부에서는 이 대리자의 +=과 -=만 사용가능 : 대리자에 사용할 함수를 추가, 삭제하는 기능을 operator +=와 -=로 구현하였다.
         public event OnContentUploadedHandler OnContentUploaded; // 대리자 선언
 
         public void UploadContent(Content content)
@@ -49,12 +49,12 @@ namespace Delegates
 
         public void Subscribe(Youtuber youtuber)
         {
-            youtuber.OnContentUploaded += Notification; // 구독하기
+            youtuber.OnContentUploaded += Notification; // 구독하기 : 대리자의 +=연산 사용
         }
 
         public void CancelSubscriptionYoutuber(Youtuber youtuber)
         {
-            youtuber.OnContentUploaded -= Notification; // 구독취소
+            youtuber.OnContentUploaded -= Notification; // 구독취소 : 대리자의 -= 연산 사용
         }
         public void Notification(Content content) {
             Console.WriteLine($"{name}님! {content.Name}이 새로 업로드 되었습니다.");
