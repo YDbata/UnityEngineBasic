40e2c5dca4d5d3bb51a7271e24beee32e85e41e2
YDbata <kiz75@naver.com>
2024-01-13 05:49:10 +0900

    이전 프로젝트 정리

--- 42D_Platformer_Game/Assets/Script/Managers/UIManager.cs
+++ 42D_Platformer_Game/Assets/Script/Managers/UIManager.cs
@@ -62,11 +62,12 @@ public class UIManager : MonoBehaviour
         if (context.started)
         {
 #if(UNITY_EDITOR || DEVELOPMENT_BUILD)
-            Debug.Log(this.name + ": " + this.GetType() + " : " +
+            Debug.Log("game" + this.name + ": " + this.GetType() + " : " +
                 System.Reflection.MethodBase.GetCurrentMethod().Name);
 #endif
+
 #if (UNITY_EDITOR)
-            UnityEditor.EditorApplication.isPlaying = false;
+    UnityEditor.EditorApplication.isPlaying = false;
 #elif (UNITY_STANDALONE)
     Application.Quit();
 #elif (UNITY_WEBGL)
